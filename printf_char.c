217d5aa6e407097b9a0184845039fc1879bf480d
evanicela <niyonizigiye.celestin@gmail.com>
2024-01-31 10:53:34 -0800

    0x11. C - printf

--- printf_char.c
+++ printf_char.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * printf_char - prints a char.
- * @val: arguments.
- * Return: 1.
+ * printf_char - prints a binary number
+ * @args: the arguements to use
+ * @printed: the printed characters
+ * Return: printed charcaters
  */
-int printf_char(va_list val)
-{
-	char s;
 
-	s = va_arg(val, int);
-	_putchar(s);
-	return (1);
+int printf_char(va_list args, int printed)
+{
+	int character = va_arg(args, int);
+
+	_putchar(character);
+	return (printed + 1);
 }
--- printf_pointer.c
+++ printf_pointer.c
@@ -1,31 +1,50 @@
 #include "main.h"
 
-/**
- * printf_pointer - prints an hexgecimal number.
- * @val: arguments.
- * Return: counter.
- */
-int printf_pointer(va_list val)
-{
-	void *p;
-	char *s = "(nil)";
-	long int a;
-	int b;
-	int i;
+#define MAX_HEX_DIGITS 16
 
-	p = va_arg(val, void*);
-	if (p == NULL)
+/**
+ * printf_pointer - prints a binary number
+ * @args: numberof arguements
+ * @printed: the printed characters
+ * Return: printed charcaters
+ */
+
+int printf_pointer(va_list args, int printed)
+{
+	void *ptr = va_arg(args, void*);
+	unsigned long num = (unsigned long) ptr;
+	int digits = 0;
+	int i;
+	unsigned long temp = num;
+	char hex_digits[MAX_HEX_DIGITS] = "0123456789abcdef";
+	char hex[MAX_HEX_DIGITS];
+
+	while (temp != 0)
 	{
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			_putchar(s[i]);
-		}
-		return (i);
+		digits++;
+		temp /= 16;
 	}
 
-	a = (unsigned long int)p;
-	_putchar('0');
-	_putchar('x');
-	b = printf_hex_aux(a);
-	return (b + 2);
+	printed += _putchar('0');
+	printed += _putchar('x');
+
+	if (num == 0)
+	{
+		printed += _putchar('0');
+	}
+	else
+	{
+		for (i = digits - 1; i >= 0; i--)
+		{
+			int digit = num % 16;
+
+			hex[i] = hex_digits[digit];
+			num /= 16;
+		}
+		for (i = 0; i < digits; i++)
+		{
+			printed += _putchar(hex[i]);
+		}
+	}
+	return (printed);
 }
